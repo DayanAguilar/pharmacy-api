688291b0efb5d5f8db2d8f622dce7f4e420b384c
Dayan Aguilar Alcocer <152241323+DayanAguilar@users.noreply.github.com>
2025-08-17 19:56:30 -0400

    feat:Origin only from web

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ sell_service = SellService(conn)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
