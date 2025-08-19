9f2dfd7ae3f74c3da01ba0394125c5e52c2aad61
DayanAguilar <dayan.aguilar.alcoccer@gmail.com>
2025-08-18 20:39:47 -0400

    fix:Don't close the db connection

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
+import psycopg2
+from psycopg2 import pool
+from dotenv import load_dotenv
+import os
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+import datetime
+
 from product_service import ProductService
 from product import Product
 from sell_service import SellService
 from sell import Sell
-import psycopg2
-from dotenv import load_dotenv
-import os
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
 from pydantic_models import ProductIn, ProductOut, SellIn, SellOut
-import datetime
 
 load_dotenv()
 POSTGRESS_SQL_URL = os.environ["POSTGRESS_SQL_URL"]
 
-conn = psycopg2.connect(POSTGRESS_SQL_URL)
-product_service = ProductService(conn)
-sell_service = SellService(conn)
+connection_pool = psycopg2.pool.SimpleConnectionPool(
+    minconn=1, 
+    maxconn=10, 
+    dsn=POSTGRESS_SQL_URL
+)
+
+if not connection_pool:
+    raise Exception("Error creando el pool de conexiones")
 
 app = FastAPI()
 app.add_middleware(
@@ -28,9 +35,18 @@ app.add_middleware(
 )
 
 
-@app.get("/products")
-def get_products():
+def get_conn():
+    conn = connection_pool.getconn()
     try:
+        yield conn
+    finally:
+        connection_pool.putconn(conn)
+
+
+@app.get("/products")
+def get_products(conn=Depends(get_conn)):
+    try:
+        product_service = ProductService(conn)
         products = product_service.get_all_products()
         return [p.to_dict() for p in products]
     except Exception as e:
@@ -38,8 +54,9 @@ def get_products():
 
 
 @app.post("/products", response_model=ProductOut)
-def create_product(product: ProductIn):
+def create_product(product: ProductIn, conn=Depends(get_conn)):
     try:
+        product_service = ProductService(conn)
         product_obj = Product(
             product_id=None,
             category=product.category,
@@ -59,8 +76,9 @@ def create_product(product: ProductIn):
 
 
 @app.put("/products/{product_id}", response_model=ProductOut)
-def update_product(product_id: int, product: ProductIn):
+def update_product(product_id: int, product: ProductIn, conn=Depends(get_conn)):
     try:
+        product_service = ProductService(conn)
         existing_product = product_service.get_product_by_id(product_id)
         if not existing_product:
             raise HTTPException(status_code=404, detail="Product not found")
@@ -83,8 +101,9 @@ def update_product(product_id: int, product: ProductIn):
 
 
 @app.delete("/products/{product_id}")
-def delete_product(product_id: int):
+def delete_product(product_id: int, conn=Depends(get_conn)):
     try:
+        product_service = ProductService(conn)
         rows_deleted = product_service.delete_product(product_id)
         if rows_deleted == 0:
             raise HTTPException(status_code=404, detail="Product not found")
@@ -96,32 +115,37 @@ def delete_product(product_id: int):
 
 
 @app.post("/sells", response_model=SellOut)
-def create_sell(sell: SellIn):
+def create_sell(sell: SellIn, conn=Depends(get_conn)):
     try:
-        product = ProductService(conn).get_product_by_id(sell.product_id)
+        product_service = ProductService(conn)
+        sell_service = SellService(conn)
+
+        product = product_service.get_product_by_id(sell.product_id)
         if not product:
             raise HTTPException(status_code=404, detail="Product not found")
 
         total_price = sell.quantity * product.sell_price
         sell_date = datetime.datetime.now().date()
         sell_obj = SellOut(
-            id=None,  
+            id=None,
             product_id=sell.product_id,
             date=sell_date,
             quantity=sell.quantity,
             total_price=total_price,
             product=product.product
         )
-        sell_id = sell_service.create_sell(sell_obj)  
+        sell_id = sell_service.create_sell(sell_obj)
         sell_obj.sell_id = sell_id
 
         return sell_obj
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+
 @app.get("/sells/{date}")
-def get_sells_by_date(date: str):
+def get_sells_by_date(date: str, conn=Depends(get_conn)):
     try:
+        sell_service = SellService(conn)
         date_obj = datetime.datetime.strptime(date, "%Y-%m-%d").date()
         sells = sell_service.generate_sell_report_by_date(date_obj)
         if sells is None:
